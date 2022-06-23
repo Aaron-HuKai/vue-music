02197283ad4359a41bf64f7a59fc172feb9f4fc0
Hk <2251497522@qq.com>
2022-06-23 19:24:58 +0800

    第一次提交“ git commit -m 第一次提交“

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <transition name="el-fade-in">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  beforeCreate() {
+    this.$store.dispatch('getAcount')
+    console.log('即将拥有8块腹肌的HK')
+  }
+}
+</script>
+
